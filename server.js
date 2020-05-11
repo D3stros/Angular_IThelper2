0396de962f713f91183c471274536ec0d113bac1
D3stros <patrick.fallmann@hotmail.com>
2020-05-11 21:56:35 +0200

    1

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use("/api", api);
 app.get("/", function (req, res) {
-  res.sendFile("AngularAuth/dist/index.html");
+  res.send("Hello from server");
 });
 
 app.listen(PORT, function () {
